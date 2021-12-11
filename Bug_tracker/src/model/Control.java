8b67d1e81da83810a82c9010dbf0c1534ec3b9c2
mahiaash <mahiaash@buffalo.edu>
2021-12-11 17:30:46 -0500

    Creation of Bug Tracker

---
+++ Bug_tracker/src/model/Control.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author mahia
+ *
+ */
+public class Control {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
