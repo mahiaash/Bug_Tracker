d5c2c59357704fe9eb73807c969b10c268bbc667
mahiaash <mahiaash@buffalo.edu>
2022-01-29 17:12:19 -0500

    Completed the look of the UI for the app

--- Bug_tracker/src/view/GUI.java
+++ Bug_tracker/src/view/GUI.java
@@ -5,15 +5,12 @@ package view;
 
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-//import javax.swing.JTextField;
 import javax.swing.SpringLayout;
-
-import java.awt.Dimension;
-import java.awt.event.*;
 
 /**
  * @author mahia
@@ -24,33 +21,60 @@ public class GUI {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// This is where the UI will go
-		String[] labels = {"Summary: ", "Location: ", "Bug Level: "}; 
+	public static void createUI() {
+		String [] labels = {"Summary: ", "Bug Location: ", "Bug Level: ", " "};
+		int labelSize = labels.length;
+		System.out.println(labelSize);
+		String [] dropDown = {"Select","Important", "To Do", "Unimportant"};
 		JFrame frame = new JFrame();
-		JButton jbutton = new JButton();
-		jbutton.setText("Submit");
-		JPanel controller = new JPanel(new SpringLayout());
-		for(int i = 0; i < 3; i++) {
-			JLabel jlabel = new JLabel(labels[i], JLabel.TRAILING);
-			controller.add(jlabel);
-			JTextArea tarea = new JTextArea(10,10);
-			tarea.setLineWrap(true);
-			jlabel.setLabelFor(tarea);
-			controller.add(tarea);
-		}
-		controller.add(jbutton);
-		
-		frame.setContentPane(controller);
-		SpringUtility.makeCompactGrid(controller,3,2,6,6, 3, 3);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		controller.setOpaque(true);
-		frame.setContentPane(controller); 
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true); 
 		frame.setSize(500, 500);
+		
+		
+		
+		JPanel controller = new JPanel();
+		controller.setLayout(new SpringLayout());
+		
+		for(int i = 0; i < labelSize; i++) {
+			JLabel label = new JLabel(labels[i], JLabel.TRAILING);
+			controller.add(label);
+			if(i == 0 || i == 1) {
+			JTextArea jtext = new JTextArea(5,5);
+			controller.add(jtext);
+			}
+			if(i == 2) {
+				JComboBox<String> jcbox = new JComboBox<String>(dropDown);
+				jcbox.setSelectedIndex(0);
+				controller.add(jcbox);
+			}
+			if(i == 3) {
+			JButton jbutton = new JButton();
+			jbutton.setText("Submit");
+			controller.add(jbutton);
+		
+			}
+		}
+		
+		Form.makeCompactGrid(controller, labelSize, 2, 6, 6, 6, 6);
+		
+		
+		
+		
+		
+		frame.setContentPane(controller);
+		controller.setOpaque(true);
+		frame.setContentPane(controller);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createUI();
+			}
+		});
 		
 
 }
